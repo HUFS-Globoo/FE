54a8dbd78319ebec67841ba46b28df54141865a3
이지우 <dlwldn041020@hufs.ac.kr>
2025-11-08 15:01:56 +0900

    Feat: axiosInstance 수정

--- src/pages/signup/SignUp4.tsx
+++ src/pages/signup/SignUp4.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import SubmitButton from '../../components/SubmitButton';
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import axiosInstance from "../../../axiosInstance";
 
 const Container = styled.div`
   width: 100%;
